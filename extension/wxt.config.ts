779a3197b4dd5096062402d9b279dae2278f3dea
VIKAS <vikasraw17@gmail.com>
2024-09-25 11:00:27 +0530

    commit

--- extension/wxt.config.ts
+++ extension/wxt.config.ts
@@ -1,6 +1,26 @@
-import { defineConfig } from 'wxt';
+import { defineWxtConfig } from 'wxt';
 
-// See https://wxt.dev/api/config.html
-export default defineConfig({
-  modules: ['@wxt-dev/module-react'],
+export default defineWxtConfig({
+  manifest: {
+    name: 'LinkedIn AI Reply',
+    version: '1.0',
+    manifest_version: 3,
+    permissions: [
+      'activeTab',
+      'scripting',
+      'storage',
+    ],
+    action: {
+      default_popup: './src/popup/index.html',
+    },
+    content_scripts: [
+      {
+        matches: ['https://www.linkedin.com/*'],
+        js: ['./src/contentScript.ts'],
+      },
+    ],
+    background: {
+      service_worker: './src/background.ts',
+    },
+  },
 });

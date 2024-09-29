fb969cf6bda37e99a6f0040feeabf9890e51f5ca
VIKAS <vikasraw17@gmail.com>
2024-09-29 13:18:13 +0530

    commit

--- extension/entrypoints/content.ts
+++ extension/entrypoints/content.ts
@@ -1,14 +1,13 @@
-// Function to insert text into the LinkedIn message input field
-function insertTextToInputField(text: string) {
-  const messageInput = document.querySelector('div.msg-form__contenteditable'); // Adjust selector for LinkedIn
-  if (messageInput) {
-      messageInput.innerHTML += text; // Insert the generated response
-  }
-}
+// If you need to export for some reason
+export default () => {
+  console.log("Content script loaded");
 
-// Listen for messages from the popup
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === 'insertResponse') {
-      insertTextToInputField(request.response);
-  }
-});
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+      if (request.action === 'insertResponse') {
+          const messageInput = document.querySelector('div.msg-form__contenteditable'); // Adjust selector for LinkedIn
+          if (messageInput) {
+              messageInput.innerHTML += request.response; // Insert the generated response
+          }
+      }
+  });
+};
